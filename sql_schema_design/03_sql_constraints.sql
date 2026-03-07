ce8e7d821952127e1b6880fb28bcdcf08996b7d0
Leebechi Juliet <lebechukwu24@gmail.com>
2026-03-07 17:06:42 +0100

    Add comments on SQL constraints
    Added comments explaining SQL constraints and their usage.

--- sql_schema_design/03_sql_constraints.sql
+++ sql_schema_design/03_sql_constraints.sql
@@ -1,1 +1,6 @@
-
+/*
+SQL constraints are used to specify rules for data in a table
+Constraints can be specified at the point of table creation with CREATE TABLE statement or 
+After table creation with ALTER TABLE statement
+Constraints determine the type of data that can go into a table, ensuring its accuracy and reliability. 
+*/
