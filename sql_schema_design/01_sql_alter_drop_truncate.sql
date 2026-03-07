d30d5501d28caf02387a72b7e1fb66cc69250e4a
Leebechi Juliet <lebechukwu24@gmail.com>
2026-03-07 17:00:19 +0100

    Update SQL statements for altering and managing tables
    Added, modified, and dropped columns in phone_accessories2 table. Included DROP and TRUNCATE statements for database management.

---
+++ sql_schema_design/01_sql_alter_drop_truncate.sql
@@ -0,0 +1,50 @@
+-- ALTER TABLE Statement
+-- The ALLTER TABLE statement is used to add, delete, or modify an existing table. 
+-- It is also used to add and drop various constraints on an existing table.
+
+-- ALTER TABLE - ADD COLUMN
+ALTER TABLE phone_accessories2
+ADD COLUMN item_type VARCHAR(30);
+
+ALTER TABLE phone_accessories2
+ADD item_price decimal(10,2);
+
+-- Verify the table alter
+DESCRIBE phone_accessories2;
+-- You should see the new columns added and the data types.
+
+-- ALTER TABLE - DROP COLUMN
+ALTER TABLE phone_accessories2
+DROP COLUMN item_type;
+-- drops a single column
+
+ALTER TABLE phone_accessories2
+DROP COLUMN item_type,
+DROP COLUMN item_price;
+-- drops twoncolumns in a single SQL statement.
+
+-- ALTER TABLE - MODIFY COLUMN
+ALTER TABLE phone_accessories2
+MODIFY COLUMN item_price DECIMAL(6,2);
+
+ALTER TABLE phone_accessories2
+MODIFY COLUMN item_id BIGINT;
+-- changed the data type of the specified column.
+
+
+-- DROP Statement
+-- IN DROP Statement, we have DROP TABLE AND DROP DATABASE
+
+-- DROP DATABASE is used to drop an existing SQL Database.
+DROP DATABASE lutech;
+-- drops the database
+
+-- The DROP TABLE statement is used to delete an existing table in a database. Dropping a table will result in loss of complete information stored in the table.
+DROP TABLE phone_accessories;
+-- deletes the table and every data stored in it.
+
+
+-- TRUNCATE Statement
+-- The TRUNCATE TABLE statement deletes the data inside a table, but not the table itself.
+TRUNCATE TABLE phone_accessories;
+
